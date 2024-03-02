68246f7a0939b5a6df56fc3b8737a6f520df59a3
Vishvjita Savkare <126576508+vishvjita@users.noreply.github.com>
2024-03-02 17:43:13 +0530

    Delete HACKSPRINT directory

--- HACKSPRINT/script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript function to toggle the navigation menu
-
-function openNavbar() {
-    const navMenu = document.getElementById("nav-menu");
-    navMenu.classList.toggle("active");
-    }
-    
